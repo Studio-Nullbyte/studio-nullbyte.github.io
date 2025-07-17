507a1e5e6095152c6e79e884d40b56831aab7c66
skaschimer <steve,kaschimer@slalom.com>
2025-07-17 11:17:23 -0400

    updared to align with new reactjs instructions

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
   },
   build: {
     outDir: 'dist',
